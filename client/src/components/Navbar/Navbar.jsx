e012f4797ece7722e3ae78665e0a6f01e983c745
Meryem L. <82731366+L-Meryem@users.noreply.github.com>
2025-11-24 15:51:14 -0500

    Add logout and responsive containers

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,30 +1,45 @@
 import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import SearchBar from './Search';
+import { useNavigate } from 'react-router-dom';
+import LogoutIcon from '../../assets/exit.png'
 
-const Navbar = ({isLogin, userName})=> {
+const Navbar = ({ isLogin, userName }) => {
+    const navigate = useNavigate();
+
+    const logout = async () => {
+        try {
+            const res = await fetch('/api/auth/logout', {
+                method: 'POST',
+                credentials: 'include'
+            });
+            const data = await res.json();
+            if (data.success)
+                navigate("/guest");
+            else
+                console.log(error);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
     return (
         <nav className="border split-nav">
             <div className="nav-brand">
                 <h3><Link to="#">Cloaked Encounter</Link></h3>
             </div>
-            {isLogin && (<SearchBar />)}
             {isLogin && (
-                 <div className="collapsible">
-                <input id="collapsible1" type="checkbox" name="collapsible1" />
-                <label htmlFor="collapsible1">
-                    <div className="bar1"></div>
-                    <div className="bar2"></div>
-                    <div className="bar3"></div>
-                </label>
-                <div className="collapsible-body">
-                    <ul className="inline">
-                        <li>{userName}</li>
-                    </ul>
-                </div>
-            </div>
+                    <div >
+                        <ul className="menu">
+                            <li><SearchBar/></li>
+                            <li className='user border'>{userName}</li>
+                            <li><NavLink onClick={logout}>
+                                <img className="exit" src={LogoutIcon} alt="Logout"/>
+                            </NavLink></li>
+                        </ul>
+                    </div>
             )}
-           
+
         </nav>
     )
 }
