3877e5ebdb6c5ef588b83a4fcaa6a623c0490df5
Meryem L. <82731366+L-Meryem@users.noreply.github.com>
2025-12-02 12:46:09 -0500

    Fix base path for Render

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,16 +4,16 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/cloaked-encounters-v2/',
+  base:'/',
   server: {
-    // host: '0.0.0.0',
-    // port: 8000,
-    // proxy: {
-    //   '/api': {
-    //     target: 'http://localhost:8080',
-    //     changeOrigin: true,
-    //     rewrite: (path) => path.replace(/^\/api/, '')
-    //   }
-    // }
+    host: '0.0.0.0',
+    port: 8000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
   }
 })
