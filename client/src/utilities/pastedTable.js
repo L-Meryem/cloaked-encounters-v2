a51376eb9e02e7070d2d941bb65026ce006de3c1
Meryem L. <82731366+L-Meryem@users.noreply.github.com>
2025-12-03 14:10:00 -0500

    Paste tables with multiple columns

--- client/src/utilities/pastedTable.js
+++ client/src/utilities/pastedTable.js
@@ -1,9 +1,10 @@
-const  pastedTableText = text => {
+const pastedTableText = text => {
     const lines = text.split('\n')
         .map(line => line.trim())
         .filter(l => l.length);
 
-    const table =  lines.map((line, index) => {
+    const table = lines.flatMap(line => (line.match(/\d+\s+[^\d]+/g) || []))
+    map((line, index) => {
         const entryText = line.replace(/^\d+\s*[\.\)\-]?\s*/, '');
 
         return {
