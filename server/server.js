da3506142ca5a77767584d98b5c91dc28780e262
Meryem L. <82731366+L-Meryem@users.noreply.github.com>
2025-12-03 11:22:28 -0500

    Allow cookies sharing across subdomains

--- server/server.js
+++ server/server.js
@@ -37,6 +37,7 @@ app.use(
             sameSite: 'none',
             httpOnly: true,
             maxAge: 3600000 * 24, // 24h
+            domain: '.cloaked-encounters.onrender.com'
         }
     })
 );
